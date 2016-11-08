05215389d335e66d814bdef3e7c448220e093ce2
christwy <wangyong069@126.com>
2016-11-08 16:10:31 +0800

    Revert "Revert "测试""
    This reverts commit 24f2a5ea406bc30d8cf7f427dd21ab283fc03b23.

---
+++ src/handler/geopen/Parameters.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package handler.geopen;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Parameters {
+
+    public static int[] map1 = new int[]{1, 2, 3, 4, 5, 6, 7};
+    public static int[] map2 = new int[]{1, 2, 4, 8, 16, 32, 64};
+    public static int[] fundmental = new int[]{1, 2, 4, 8};
+    public static String[] arrays = new String[]{"", "大定源", "重叠回线", "中心回线", "电性线源", "偶极源"};
+    public static int channels = 0;//设定道数
+    public static int files = -1;
+    public static int gain = -1;
+    public static int superp = -1;
+    public static int fund = -1;
+    public static int repeat = -1;
+    public static double ramp = -1;
+    public static double area = -1;
+    public static double cuurent = -1;
+    public static double startT = -1;
+    public static double endT = -1;
+    public static int wins = -1;
+    public static int midLat1 = -1;
+    public static double midLat2 = -1;
+    public static int midLong1 = -1;
+    public static int latIndex = 0;
+    public static int longIndex = 0;
+    public static double midLong2 = -1;
+    public static double transmitedArea = -1;
+    public static int arrayIndex = 0;
+    public static String[] channelsEH = null;//通道定义
+    public static int restrainFrequency = 0;//抑制频率
+}
